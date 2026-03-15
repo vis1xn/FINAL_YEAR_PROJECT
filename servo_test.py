7e9aaf87fbced9f012884c74b1ff4554847f5976
Tyrese <g00421432@atu.ie>
2026-03-15 17:48:24 +0000

    Working servo config

--- servo_test.py
+++ servo_test.py
@@ -1,12 +1,25 @@
-from gpiozero import AngularServo
-from time import sleep
-
-servo  = AngularServo(18, min_angle=-90, max_angle=90)
-
-while True:
-    servo.angle = -90
-    sleep(1)
-    servo.angle = 0
-    sleep(1)
-    servo.angle = (90)
-    sleep(1)
+import RPi.GPIO as GPIO
+import time
+SERVO_PIN = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setwarnings(False)
+pwm = GPIO.PWM(SERVO_PIN, 50)
+pwm.start(0)
+def set_angle(angle):
+   duty = 1 + (angle / 18)
+   pwm.ChangeDutyCycle(duty)
+   time.sleep(0.8)
+   pwm.ChangeDutyCycle(0)
+   time.sleep(0.5)
+print("Testing servo sweep...")
+for angle in [0, 45, 90, 135, 180]:
+   print(f"Moving to {angle} degrees")
+   set_angle(angle)
+print("Sweeping back...")
+for angle in [180, 135, 90, 45, 0]:
+   print(f"Moving to {angle} degrees")
+   set_angle(angle)
+pwm.stop()
+GPIO.cleanup()
+print("Done")
